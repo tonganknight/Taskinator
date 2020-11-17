cf40966f5782e6b30c335639105ebf38b68901ec
Malachi <malachialusa@gmail.com>
2020-11-17 16:11:45 -0700

    added base work for input options to custimize tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,37 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 //this function adds a task to 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+var createTaskHandler = function(event) {
+    event.preventDefault();//this ensures the browser will not refresh during the submit
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+         // create list item
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+
+        // create div to hold task info and add to list item
+        var taskInfoEl = document.createElement("div");
+        // give it a class name
+        taskInfoEl.className = "task-info";
+
+
+        // add HTML content to div
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+      
+        listItemEl.appendChild(taskInfoEl);
+
+        // add entire list item to list
+        tasksToDoEl.appendChild(listItemEl);
+      
+
+    
+
   }
 
 
-  buttonEl.addEventListener("click", createTaskHandler)
+  formEl.addEventListener("submit", createTaskHandler);
 
 

bce52e8a537e869d6e39b85bde5b59d7be582300
Malachi <malachialusa@gmail.com>
2020-11-18 15:46:32 -0700

    Added click and drag features

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,6 +97,7 @@ listItemEl.className = "task-item";
 
 // add task id as a custom attribute
 listItemEl.setAttribute("data-task-id", taskIdCounter);
+listItemEl.setAttribute("draggable", "true");
 
 // create div to hold task info and add to list item
 var taskInfoEl = document.createElement("div");
@@ -209,12 +210,68 @@ var createTaskActions = function(taskId) {
 
   };
   
-  
+  var dragTaskHandler = function(event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    event.dataTransfer.setData("text/plain", taskId);
+    var getId = event.dataTransfer.getData("text/plain");
+    console.log("getId:", getId, typeof getId);
+}
+
+  var dropZoneDragHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+
+      event.preventDefault();
+      
+      dropZoneEl.removeAttribute("style");
+    }
+  };
+
+
+  var dropTaskHandler = function(event) {
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+    // set status of task based on dropZone id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+    if (statusType === "tasks-to-do") {
+      statusSelectEl.selectedIndex = 0;
+    } 
+    else if (statusType === "tasks-in-progress") {
+      statusSelectEl.selectedIndex = 1;
+    } 
+    else if (statusType === "tasks-completed") {
+      statusSelectEl.selectedIndex = 2;
+    }
+
+    dropZoneEl.appendChild(draggableElement);
+
+  };
+
+    var dragLeaveHandler = function(event) {
+      var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+     taskListEl.removeAttribute("style");
+}
+
+    }
+
+
+  pageContentEl.addEventListener("dragleave", dragLeaveHandler);
+
+  pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+
+  pageContentEl.addEventListener("dragstart", dragTaskHandler);
 
   pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
   pageContentEl.addEventListener("click", taskButtonHandler);
 
   formEl.addEventListener("submit", taskFormHandler);
+
+  pageContentEl.addEventListener("drop", dropTaskHandler); // maybe needs to be at top
 
 
